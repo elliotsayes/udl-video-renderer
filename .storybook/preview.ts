2ac7cda6b574906a919248763d64c9eb175b2904
Elliot Sayes <elliot@sayes.cloud>
2023-11-05 11:30:17 +0700

    Theme switching

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,5 @@
 import type { Preview } from "@storybook/react";
+import { withThemeByClassName } from "@storybook/addon-themes";
 import "../src/index.css";
 
 const preview: Preview = {
@@ -11,6 +12,15 @@ const preview: Preview = {
       },
     },
   },
+  decorators: [
+    withThemeByClassName({
+      themes: {
+        light: "light",
+        dark: "dark",
+      },
+      defaultTheme: "light",
+    }),
+  ],
 };
 
 export default preview;
