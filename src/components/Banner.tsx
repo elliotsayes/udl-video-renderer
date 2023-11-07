2cb528df6d2e61375c74150b607f954d88e3247a
Elliot Sayes <elliot@sayes.cloud>
2023-11-08 01:18:17 +0700

    tweak height

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -11,7 +11,7 @@ export const Banner = () => {
   const address = useActiveAddress();
 
   return (
-    <div className="flex flex-row flex-grow-0 items-center justify-between bg-gradient-to-b from-primary/10 to-primary/5 py-2 px-4 h-14">
+    <div className="flex flex-row flex-grow-0 items-center justify-between bg-gradient-to-b from-primary/10 to-primary/5 py-2 px-4 h-16">
       <span className="text-xl font-semibold leading-none tracking-tight">
         {AppName}
       </span>
