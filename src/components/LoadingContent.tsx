7e1d7cf17de1ac2134e0828edf1b1489e91e254b
Elliot Sayes <elliot@sayes.cloud>
2023-11-08 09:38:00 +0700

    fix element

--- src/components/LoadingContent.tsx
+++ src/components/LoadingContent.tsx
@@ -8,9 +8,9 @@ export const LoadingContent: React.FC<Props> = ({ children }) => {
   return (
     <div className="h-[100%] flex flex-col items-center justify-center bg-primary-foreground z-50">
       <div className="animate-spin h-20 w-20 rounded-full border-t-2 border-b-2 border-primary"></div>
-      <p className="mt-4 text-primary/80">
+      <div className="mt-4 text-primary/80">
         {children}
-      </p>
+      </div>
     </div>
   );
 };
