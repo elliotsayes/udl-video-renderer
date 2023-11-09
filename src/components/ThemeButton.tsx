a1dcec2012b5969b326f0f2e82166d26e463c934
Elliot Sayes <elliot@sayes.cloud>
2023-11-09 13:27:01 +0700

    Invert theme icons

--- src/components/ThemeButton.tsx
+++ src/components/ThemeButton.tsx
@@ -18,9 +18,9 @@ export const ThemeButton = () => {
     >
       {
         activeTheme === "light" ? (
-          <SunIcon />
-        ) : (
           <MoonIcon />
+        ) : (
+          <SunIcon />
         )
       }
     </Button>
